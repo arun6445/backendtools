616d7c77205eeddfb0702e6454a299ae334a107a
SergeWilfried <sergewilfried@outlook.fr>
2021-03-17 18:13:28 -0400

    added buildspecs.yml

--- bin/deploy-new-version-aws.sh
+++ bin/deploy-new-version-aws.sh
@@ -3,10 +3,10 @@ if [[ $# -eq 0 ]] ; then
     echo 'Please pass the number of new version'
     exit 0
 fi
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 276472736030.dkr.ecr.us-east-1.amazonaws.com
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 755297683773.dkr.ecr.us-east-1.amazonaws.com
 
 docker build -t duniapay-api:$1 .
 
-docker tag duniapay-api:$1 276472736030.dkr.ecr.us-east-1.amazonaws.com/duniapay-api:$1
+docker tag duniapay-api:$1 755297683773.dkr.ecr.us-east-1.amazonaws.com/duniapay-api:$1
 
-docker push 276472736030.dkr.ecr.us-east-1.amazonaws.com/duniapay-api:$1
+docker push 755297683773.dkr.ecr.us-east-1.amazonaws.com/duniapay-api:$1
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe, BadRequestException } from '@nestjs/common';
+import * as Sentry from '@sentry/node';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  Sentry.init({
+    dsn: 'https://9bc8655b736549d6a08d45e1e809e852@o452631.ingest.sentry.io/5569231',
+  })
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
