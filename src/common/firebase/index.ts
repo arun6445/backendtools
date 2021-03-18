70a09bb95baafa52690169197d5544af56945b44
arlovaalina <arlovaalina@gmail.com>
2021-03-18 20:23:52 +0300

    Add firebase account key to s3

--- src/common/firebase/index.ts
+++ src/common/firebase/index.ts
@@ -1,12 +1,18 @@
 import FirebaseService from './firebase.service';
 
-import * as serviceAccount from './config/serviceAccountKey.json';
+import S3Service from 'common/s3/s3.service';
 import { ServiceAccount } from 'firebase-admin';
 
 export const firebaseFactory = {
   provide: FirebaseService,
-  useFactory: () => {
-    return new FirebaseService(serviceAccount as ServiceAccount);
+  useFactory: async (s3Service: S3Service) => {
+    const serviceAccount = await s3Service.getDataFromS3(
+      'firebase_account_key.json',
+    );
+
+    return new FirebaseService(
+      JSON.parse(serviceAccount.toString()) as ServiceAccount,
+    );
   },
-  inject: [],
+  inject: [S3Service],
 };
